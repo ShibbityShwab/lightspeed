a8094355eeac9e079d86585f74cbe1d16d2de8dc
ShibbityShwab <18537102+ShibbityShwab@users.noreply.github.com>
2026-09-25 21:05:45 +0700

    chore(homebrew): bump to v1.6.12 (release-job index lag workaround)

--- Formula/lightspeed.rb
+++ Formula/lightspeed.rb
@@ -1,29 +1,29 @@
 class Lightspeed < Formula
   desc "Zero-cost global network optimizer for multiplayer games"
   homepage "https://github.com/ShibbityShwab/lightspeed"
-  version "1.6.11"
+  version "1.6.12"
   # Custom noncommercial license (LightSpeed-NC-1.0), not an SPDX identifier.
   license :cannot_represent
 
   on_macos do
     on_intel do
-      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.11/lightspeed-client-x86_64-apple-darwin.tar.xz"
-      sha256 "6b6c4d19143d82c0cde7bb0806fdec77b2b85d23fe88720438b283f4d79d0d5d"
+      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.12/lightspeed-client-x86_64-apple-darwin.tar.xz"
+      sha256 "1884ae2d62dab4b5e5d094e19dd7cdcce4ce6c337e342f6164de770f9a598edc"
     end
     on_arm do
-      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.11/lightspeed-client-aarch64-apple-darwin.tar.xz"
-      sha256 "297db946e2dfa293e613373eef171d968aec7ff449e1a33670802fe09e62aed6"
+      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.12/lightspeed-client-aarch64-apple-darwin.tar.xz"
+      sha256 "68f416f93eaf9e8d8f50d0d7a35761d46e22725117311bb0d586309c6e10f8e8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.11/lightspeed-client-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "c31a98dd77eafd08665ee816226fa9b7f92c5629740b258ce981329c8f958e1b"
+      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.12/lightspeed-client-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "d0bfab6022b231296e985ff5eee0464ddb0775894d6aa607079f40e1a7a2a81f"
     end
     on_arm do
-      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.11/lightspeed-client-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "adb3a3bf05e50fb9165b53c2a58016b84ef81273b4534f46552eabe61fa3f9cc"
+      url "https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.12/lightspeed-client-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "db5621fe613bcadea64ae8d6d8c7d736e7e9039bd8c476be30183dd56863d1e3"
     end
   end
 
