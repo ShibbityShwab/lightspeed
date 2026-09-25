910852016d47704ec90d7e79c79c7b319b80ba18
ShibbityShwab <18537102+ShibbityShwab@users.noreply.github.com>
2026-09-25 18:53:21 +0700

    chore(chocolatey): bump to v1.6.11 (release-job index lag workaround)

--- dist/chocolatey/tools/chocolateyInstall.ps1
+++ dist/chocolatey/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.10/lightspeed-client-x86_64-pc-windows-msvc.zip'
-  checksum64     = '82ee0342eaf672a8985e88944d80de2085e17eb55b47c4687fd89452ced9f9ab'
+  url64bit       = 'https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.11/lightspeed-client-x86_64-pc-windows-msvc.zip'
+  checksum64     = 'ba8a160680baaef402e3f95b473d7757cc647759064c9d618692630b9fa92399'
   checksumType64 = 'sha256'
 }
 
