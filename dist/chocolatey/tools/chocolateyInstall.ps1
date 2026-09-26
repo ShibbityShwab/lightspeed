c89274712b6a75d2f9f3be4d27d4cfc9e8fef0bc
ShibbityShwab <18537102+ShibbityShwab@users.noreply.github.com>
2026-09-27 00:53:00 +0700

    chore(chocolatey): bump to v1.6.14

--- dist/chocolatey/tools/chocolateyInstall.ps1
+++ dist/chocolatey/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.13/lightspeed-client-x86_64-pc-windows-msvc.zip'
-  checksum64     = '37ae9f8f4ed3950379061aea6f0142424953b23f8ceebbbef6a4e21ec26d2276'
+  url64bit       = 'https://github.com/ShibbityShwab/lightspeed/releases/download/v1.6.14/lightspeed-client-x86_64-pc-windows-msvc.zip'
+  checksum64     = '1b8e969feb84b2ac17b2732fcb569c8c91926800cd425d75f80b96e53d234d25'
   checksumType64 = 'sha256'
 }
 
